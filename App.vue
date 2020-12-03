f948349bfa0ef24d5b9dd37f7384255820aefbd8
ericnoel96 <eric.noel@udla.edu.ec>
2020-12-03 01:07:09 -0500

    fisrt commit

---
+++ App.vue
@@ -0,0 +1,48 @@
+<template>
+  <app-navigator></app-navigator>
+</template>
+
+<script>
+import {
+  createAppContainer,
+  createMaterialTopTabNavigator,
+  createStackNavigator
+} from "vue-native-router";
+
+import HomeScreen from "./screens/Home.vue";
+import CameraScreen from "./screens/Camera.vue";
+import WebServiceScreen from "./screens/WebService.vue";
+import GeolocationScreen from "./screens/Geolocation.vue";
+import DatabaseScreen from "./screens/Database.vue";
+
+const Tabs = createMaterialTopTabNavigator({
+  geolocalizacion: GeolocationScreen,
+});
+
+const StackNavigator = createStackNavigator(
+  {
+    Tabs,
+    Camara: CameraScreen
+  },
+  {
+    defaultNavigationOptions: {
+      gestureEnabled: false,
+      title: "EA-NR - DEBER",
+      headerTitleStyle: {
+        textAlign: "center",
+        alignSelf: "center",
+        color: "white"
+      },
+      headerStyle: {
+        backgroundColor: "#088A85"
+      }
+    }
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+  components: { AppNavigator }
+};
+</script>
